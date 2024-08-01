87f7d3fdd69cc76c742986321e30d08c9d7046e3
Andrei Livadariu <livadariu.andrei@gmail.com>
2024-08-01 14:13:43 +0200

    Fix - Don't skip today when announcing weekly events

--- modules/events.py
+++ modules/events.py
@@ -95,7 +95,7 @@ class EventsModule(BaseModule):
         if not self.announcement_chats:
             return
 
-        upcoming_text = self._format_upcoming(datetime.now(self.timezone), self.upcoming_days + 1)
+        upcoming_text = self._format_upcoming(datetime.now(self.timezone), self.upcoming_days)
 
         # Don't announce anything if there are no upcoming events
         if not upcoming_text:
